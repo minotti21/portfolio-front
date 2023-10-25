ce404e12bbed534ddc36ef877c822cda0757743b
Vinicius Minotti <vinicius.minotti21@gmail.com>
2023-10-25 07:34:03 -0300

    adding images

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import SectionContainer from "@/components/Section";
-import { AboutContainer, StyledText, StyledTitle, TextAndImageContainer } from "./styles";
+import { AboutContainer, IconsContainer, StyledImage, StyledSkillsText, StyledText, StyledTitle, TextAndImageContainer, TextContainer } from "./styles";
+import Image from "next/image";
 
 export default function About() {
     return (
@@ -9,14 +10,41 @@ export default function About() {
             <AboutContainer>
                 <StyledTitle>Minha história</StyledTitle>
                 <TextAndImageContainer>
-                    <StyledText>Minha jornada no mercado de trabalho começou aos 18 anos em 2019, quando entrei na empresa Lupo S/A para trabalhar como menor aprendiz de auxiliar administrativo! Fui efetivado após seis meses e segui desenvolvendo minhas habilidades.</StyledText>
+                    <TextContainer>
+                        <StyledText>Minha jornada no mercado de trabalho começou aos 18 anos em 2019, quando entrei na empresa Lupo S/A para trabalhar como menor aprendiz de auxiliar administrativo! Fui efetivado após seis meses e segui desenvolvendo minhas habilidades.</StyledText>
+                        <StyledSkillsText>Soft Skills: Comunicação assertiva, pensamento crítico e resolução de problemas</StyledSkillsText>
+                        <StyledSkillsText>Hard Skills: SAP, Excel e Word</StyledSkillsText>
+                    </TextContainer>
+                    <StyledImage>
+                        <Image src="/img3.jpg" alt="escritório" fill={true} sizes="" style={{ borderRadius: 12 }} quality={100} />
+                        <IconsContainer>
+                            <Image src="/sap.svg" width={50} height={50} alt="icon" />
+                            <Image src="/excel.svg" width={50} height={50} alt="icon" />
+                            <Image src="/word.svg" width={50} height={50} alt="icon" />
+                        </IconsContainer>
+                    </StyledImage>
                 </TextAndImageContainer>
                 <TextAndImageContainer>
-                    <h1></h1>
-                    <StyledText>Então a pandemia entrou em nossas vidas! Após dois meses em casa jogando, alcancei um dos ranks mais altos no Brasil no jogo League of Legends, chegou o momento de eu tentar algo diferente, saí da Lupo e me dediquei totalmente para ser jogador profissional.</StyledText>
+                    <StyledImage>
+                        <Image src="/setup.jpg" alt="escritório" fill={true} sizes="" style={{ borderRadius: 12 }} quality={100} />
+                        <IconsContainer>
+                            <Image src="/sap.svg" width={50} height={50} alt="icon" />
+                            <Image src="/excel.svg" width={50} height={50} alt="icon" />
+                            <Image src="/word.svg" width={50} height={50} alt="icon" />
+                        </IconsContainer>
+                    </StyledImage>
+                    <TextContainer>
+                        <StyledText>Então a pandemia entrou em nossas vidas! Após dois meses em casa jogando, alcancei um dos ranks mais altos do Brasil no jogo League of Legends, chegou o momento de eu tentar algo diferente, saí da Lupo e me dediquei totalmente a ser jogador profissional.</StyledText>
+                        <StyledSkillsText>Soft Skills: Raciocínio rápido</StyledSkillsText>
+                        <StyledSkillsText>Hard Skills: SAP, Excel e Word</StyledSkillsText>
+                    </TextContainer>
                 </TextAndImageContainer>
                 <TextAndImageContainer>
-                    <StyledText>Nem tudo são flores, após mais ou menos um ano tentando ser contratado por um time e as reservas acabando, decidi que era hora de desistir do meu sonho e tentar outra coisa... Então conheci a programação por meio de amigos que já eram da area, me dediquei como nunca aos estudos.</StyledText>
+                    <TextContainer>
+                        <StyledText>Nem tudo são flores, após mais ou menos um ano tentando ser contratado por um time e as reservas acabando, decidi que era hora de desistir do meu sonho e tentar outra coisa... Então conheci a programação por meio de amigos que já eram da area, me dediquei como nunca aos estudos.</StyledText>
+                        <StyledSkillsText>Soft Skills: Comunicação assertiva, pensamento crítico e resolução de problemas</StyledSkillsText>
+                        <StyledSkillsText>Hard Skills: SAP, Excel e Word</StyledSkillsText>
+                    </TextContainer>
                 </TextAndImageContainer>
             </AboutContainer>
         </SectionContainer>
--- src/screens/About/styles.ts
+++ src/screens/About/styles.ts
@@ -1,3 +1,5 @@
+import { white } from "@/constants/colors";
+import rgbUtils from "@/utils/rgbUtils";
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
@@ -12,9 +14,12 @@ export const StyledTitle = styled.h1`
 `
 
 export const StyledText = styled.p`
-    flex: 0.4;
     font-size: 1.5em;
     font-weight: 400;
+`
+
+export const TextContainer = styled.div`
+    flex: 0.5;
 `
 
 export const TextAndImageContainer = styled.div`
@@ -22,4 +27,31 @@ export const TextAndImageContainer = styled.div`
     justify-content: space-between;
     align-items: center;
     margin: 4em 0;
+    gap: 6em;
 `
+
+export const StyledSkillsText = styled.p`
+    font-size: 1em;
+    color: #999;
+    margin-top: 0.4em;
+`
+
+export const StyledImage = styled.div`
+    width: 600px;
+    height: 300px;
+    position: relative;
+    flex: 0.5;
+    border-radius: 12px;
+`
+
+export const IconsContainer = styled.div`
+    position: absolute;
+    padding: 0.5em 1em;
+    bottom: -2em;
+    left: -2em;
+    display: flex;
+    gap: 2em;
+    transition: 0.3s;
+    background-color: ${({ theme }) => theme.backgroundColor === white ? rgbUtils.lightenRGBColor(theme.fontColor, 20) : rgbUtils.darkenRGBColor(theme.fontColor, 20) };
+    border-radius: 12px;
+` 
